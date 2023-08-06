91c3392e1b170fbed27c6fd3b5a4f77f62b66520
kazuto ando <kazuto.ando@gmail.com>
2023-08-06 21:27:28 +0900

    path param

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import uvicorn
+from typing import Union
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 app = FastAPI()
 
@@ -57,3 +67,17 @@ async def get_model(model_name: ModelName):
 
     return {"model_name": model_name, "message": "Have some residuals"}
     
+    
+# リクエストボディ
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# デバック用途
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
