9332f408c5c0cd3fa0b6227e8ca6d3b6c95258f8
kazuto ando <kazuto.ando@gmail.com>
2023-08-06 20:04:50 +0900

    パスパラメータ

--- main.py
+++ main.py
@@ -54,6 +54,6 @@ async def get_model(model_name: ModelName):
 
     if model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
-    
+
     return {"model_name": model_name, "message": "Have some residuals"}
     
